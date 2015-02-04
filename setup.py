00dbebcf505515794451a4f3be5d128c80dafe88
dlmeduLi <dlmeduLi@163.com>
2015-02-04 15:20:41 +0800

    version 0.1.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(fname):
 
 setup(
     name = "unique-sam",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "dlmeduLi",
     author_email = "dlmeduLi@163.com",
     description = ("Analyse sam file and keep the unique aligment record."),
